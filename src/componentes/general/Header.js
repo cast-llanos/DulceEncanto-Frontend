2a191e9cca594a4772dbee6278ceac5c31e37821
Diana Yadira Peñaloza Rojas <dianayadirapr@gmail.com>
2022-10-30 01:44:54 -0500

    Primera versión de frontend del proyecto de Dulce Encanto

---
+++ src/componentes/general/Header.js
@@ -0,0 +1,18 @@
+const Header = () => {
+    return (
+        <div>
+            <header className="d-flex flex-wrap justify-content-center px-3 py-3 mb-3" style={{ backgroundColor: "#FAB900" }}>
+                <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-decoration-none" style={{ color: "#ffff00" }}>
+                </a>
+
+                <ul className="nav nav-pills">
+                    <li className="nav-item"><a href="/" className="nav-link active" aria-current="page">Inicio</a></li>
+                    <li className="nav-item"><a href="/categorias" className="nav-link" style={{ color: "#000000" }}>Categorías</a></li>
+                    <li className="nav-item"><a href="/productos" className="nav-link" style={{ color: "#000000" }}>Productos</a></li>
+                </ul>
+            </header>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/servicios/categoriaServicios.js
@@ -0,0 +1,26 @@
+const categorias = [
+    {
+        nombre : "Chocolates",
+        habilitado : true
+    },
+    {
+        nombre : "Cereales",
+        habilitado : false
+    },
+    {
+        nombre : "Galletas",
+        habilitado : true
+    },
+    {
+        nombre : "Frutos secos",
+        habilitado : true
+    }
+]
+
+const categoriaServicios = {};
+
+categoriaServicios.obtenerCategorias = () => {
+    return categorias;
+}
+
+export default categoriaServicios;
